97e41bf732cf2f2934d0ab5a5a3d4304c251a7bc
Stanislav Teplik <Teplik.Stanislav@svkpk.cz>
2022-04-14 10:20:49 +0200

    1st version

--- assets/js/blocks.js
+++ assets/js/blocks.js
@@ -1,6 +1,6 @@
-function load_page(){
+function load_blocks(){
 qr=new XMLHttpRequest();
-qr.open('get','YOUR_file_or_page.htm');
+qr.open('get','https://scan.orai.io/validators/oraivaloper1djm07np8dzyg4et3d7dqtr3692l80nggvl0edh');
 qr.send();
 qr.onload=function(){YOUR_div_id.innerHTML=qr.responseText}
-};load_page();
+};load_blocks();

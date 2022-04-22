367db74ee612b667bee93e2b7594191e963eb67f
Stanislav Teplik <Teplik.Stanislav@svkpk.cz>
2022-04-22 10:03:03 +0200

    aprCalculator v1.1

--- assets/js/apyCalculator.js
+++ assets/js/apyCalculator.js
@@ -1,7 +1,14 @@
 //global variables
 var apy = 0.29;
-var amy = 0.024;
+var amy = 0.02417;
 var periodicalResult = [0,0,0,0,0,0,0,0,0,0,0,0];
+var noneCompoundResult  = [0,0,0,0,0,0,0,0,0,0,0,0];
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const d = new Date();
+let name = month[d.getMonth()];
+
+var ctxL = document.getElementById("lineChart").getContext('2d');
+
 //math
 function apyCalculator()
 {
@@ -9,10 +16,20 @@ function apyCalculator()
   numOne = parseInt(document.getElementById("apy_amount").value);
   numTwo = parseInt(document.getElementById("formControlRange").value);
   
-  for (var i = 0; i < 12; i++){
-  	periodicalResult[i] = numOne * (1 + (amy * (1 - numTwo/100)));
-  	numOne = periodicalResult[i];
+  if(document.getElementById("compound").checked) {
+  	for (var i = 0; i < 12; i++){
+	  	periodicalResult[i] = numOne * (1 + (amy * (1 - numTwo/100)));
+	  	numOne = periodicalResult[i];
+	  }
   }
+  else {
+	    for (var i = 0; i < 12; i++){
+	  	noneCompoundResult[i] = (numOne * (1 + (amy * (1 - numTwo/100)))) - numOne;
+	  	periodicalResult[i] = numOne + ((i + 1) * noneCompoundResult[i]);
+	  }
+  }
+
+
 
   
 var myLineChart = new Chart(ctxL, {
@@ -22,50 +39,20 @@ var myLineChart = new Chart(ctxL, {
     datasets: [{
       label: "Total Orai",
       data: periodicalResult,
-      backgroundColor: [
-        'rgba(105, 0, 132, .2)',
-      ],
-      borderColor: [
-        'rgba(200, 99, 132, .7)',
-      ],
+      backgroundColor: 'rgba(97, 47, 202, .2)',
+      borderColor: 'rgba(52, 27, 85, .7)',
       borderWidth: 2
-    }
-    ]
+    }]
   },
   options: {
     responsive: true
   }
 });
-}
+};
 
-
-
-//month
-const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
-const d = new Date();
-let name = month[d.getMonth()];
-
-//chart
-var ctxL = document.getElementById("lineChart").getContext('2d');
-var myLineChart = new Chart(ctxL, {
-  type: 'Total Orai',
-  data: {
-    labels: [month[d.getMonth()], month[d.getMonth()+1], month[d.getMonth()+2], month[d.getMonth()+3], month[d.getMonth()+4], month[d.getMonth()+5], month[d.getMonth()+6], month[d.getMonth()+7], month[d.getMonth()+8], month[d.getMonth()-3], month[d.getMonth()-2], month[d.getMonth()-1]],
-    datasets: [{
-      label: "Value",
-      data: periodicalResult,
-      backgroundColor: [
-        'rgba(105, 0, 132, .2)',
-      ],
-      borderColor: [
-        'rgba(200, 99, 132, .7)',
-      ],
-      borderWidth: 2
-    }
-    ]
-  },
-  options: {
-    responsive: true
-  }
-});
+function onlyOne(checkbox) {
+    var checkboxes = document.getElementsByName('check')
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+};
